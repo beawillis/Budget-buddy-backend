96bd63500d0eecbddbb0383179522f67786415aa
beawillis <beawillis2005@gmail.com>
2026-06-25 18:38:55 +0300

    Cors fix

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,45 +1,9 @@
-const cors = require("cors");
+const corsOptions = {
+  origin: '*', // Allows EVERYTHING
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  optionsSuccessStatus: 200
+};
 
-module.exports = cors({
-
-origin:(origin,callback)=>{
-
-if(!origin){
-return callback(null,true);
-}
-
-const allowed =
-
-origin.includes(".vercel.app") ||
-
-origin==="https://budget-buddy-fintech.vercel.app/" ||
-
-origin==="https://budget-buddy-fintech-git-main-beawillis2005-2956s-projects.vercel.app/" ||
-
-origin==="https://budget-buddy-fintech-8jtzknec1-beawillis2005-2956s-projects.vercel.app/";
-
-if(allowed){
-return callback(null,true);
-}
-
-callback(new Error("CORS blocked"));
-
-},
-
-credentials:true,
-
-methods:[
-"GET",
-"POST",
-"PUT",
-"PATCH",
-"DELETE",
-"OPTIONS"
-],
-
-allowedHeaders:[
-"Content-Type",
-"Authorization"
-]
-
-});
+module.exports = corsOptions;
